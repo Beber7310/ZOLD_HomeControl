5143d9de14d25065a2a794f3086b047f0aadee91
Bertrand <b.dosda@gmail.com>
2013-12-29 15:57:12 +0100

    adding rdd header

--- src/rdd.h
+++ src/rdd.h
@@ -11,3 +11,9 @@
 
 
 #endif /* RDD_H_ */
+
+
+void rrd_create()
+{
+	system("rrdtool create temp.rrd --start 1297810800 DS:exterieur:GAUGE:600:U:U DS:salon:GAUGE:600:U:U RRA:AVERAGE:0.5:1:24 RRA:AVERAGE:0.5:6:12");
+}
