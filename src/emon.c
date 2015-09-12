ceb27701aa8d55c8f5b5906dc82c00e9f0dc4764
Bertrand <b.dosda@gmail.com>
2015-09-12 18:50:10 +0200

    push emon

---
+++ src/emon.c
@@ -0,0 +1,36 @@
+/*
+ * emon.c
+ *
+ *  Created on: 6 févr. 2015
+ *      Author: Bertrand
+ */
+
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <time.h>
+#include <pthread.h>
+#include <stdarg.h>
+
+//
+//  http://emoncms.org/input/post.json?json={power:200}&apikey=33d19b54e3e8cc180aa03bf0eeee8518
+//
+
+
+void emon_log_data(char* name, float value)
+{
+	char cmdline[1024];
+	sprintf(cmdline,"curl \"http://emoncms.org/input/post.json?json={%s:%f}&apikey=33d19b54e3e8cc180aa03bf0eeee8518\"",name,value);
+
+	system(cmdline);
+}
---
+++ src/emon.h
@@ -0,0 +1,13 @@
+/*
+ * emon.h
+ *
+ *  Created on: 6 févr. 2015
+ *      Author: Bertrand
+ */
+
+#ifndef EMON_H_
+#define EMON_H_
+
+void emon_log_data(char* name, float value);
+
+#endif /* EMON_H_ */
